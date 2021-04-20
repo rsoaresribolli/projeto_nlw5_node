1775b743baf3060377872311114874d259d43ae5
Ricardo Soares Ribolli <ricardo.ribolli@inspira.com.br>
2021-04-20 09:23:31 -0300

    First commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+import "./database";
+
+const app = express();
+
+app.get("/", (request, response) => {
+    return response.json({
+        message: "Olá NLW 05"
+    });
+});
+
+app.post("/", (request, response) => {
+    return response.json({
+        message: "Usuário salvo com sucesso!"
+    });
+});
+
+app.listen(3333, () => console.log("Server is running on port 3333"));
